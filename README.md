8a39237d961df3263464c165fefd5dc1273759a5
Bagus Hary <bagushary79@gmail.com>
2023-12-08 11:12:01 +0700

    Menambahkan komentar pada source code

--- README.md
+++ README.md
@@ -3,26 +3,34 @@
 <img src="https://github.com/harybagus/remob/assets/126042692/ccd19d55-ac55-4f24-90ea-6bd82e9da426" width="300">
 
 ## ReMob
+
 ReMob adalah sebuah website penyedia jasa rental mobil.
 
 ## Kelompok 2
+
 Nama anggota kelompok:
-* Muhammad Nias D.K. (10220049)
-* Alvian Nur Isra (10220031)
-* Bagus Hary (10220037)
-* Buana Rizki N.H (10220058)
-* Thomas Febry Cahyono (10220063)
+
+- Muhammad Nias D.K. (10220049)
+- Alvian Nur Isra (10220031)
+- Bagus Hary (10220037)
+- Buana Rizki N.H (10220058)
+- Thomas Febry Cahyono (10220063)
 
 ## Teknologi yang digunakan
+
 Berikut teknologi yang kami gunakan untuk membuat website ini:
-* HTML
-* SB Admin 2 (template)
-* Bootstrap 4
-* JavaScript
-* Codeigniter 4
-* MySQL
+
+- HTML
+- CSS
+- SB Admin 2 (template)
+- Bootstrap 4
+- JavaScript
+- PHP
+- Codeigniter 4
+- MySQL
 
 ## Instalasi
+
 1. Clone repository ini ke dalam folder htdocs.
 2. Buka XAMPP lalu jalankan Apache dan MySQL.
 3. Import database `remob.sql` ke MySQL.
@@ -31,51 +39,62 @@ Berikut teknologi yang kami gunakan untuk membuat website ini:
 6. Buka browser favorit Anda lalu ketikkan `localhost:8080/auth`
 
 ## Akun
+
 1. Admin
+
 ```
 adminsatu@remob.co.id
 password: adminsatu
 ```
+
 ```
 admindua@remob.co.id
 password: admindua
 ```
 
 2. Penyewa
+
 ```
 mas@cahyono.my.id
 password: yonoyono
 ```
+
 ```
 bagus@banget.com
 password: bagusbanget
 ```
+
 ```
 niasaerox@gmail.com
 password: niasaerox
 ```
+
 ```
 iyan@alvian.xyz
 password: iyaniyan
 ```
+
 ```
 buana@ganteng.com
 password: buana123
 ```
 
 ## Fitur
+
 1. Admin
-* Mengubah profil dan password sendiri
-* Mengelola data admin (CRUD)
-* Mengelola data mobil (CRUD)
-* Mengelola data penyewa (CRUD)
-* Melihat data penyewaan
-* Menginput pengembalian mobil
+
+- Mengubah profil dan password sendiri
+- Mengelola data admin (CRUD)
+- Mengelola data mobil (CRUD)
+- Mengelola data penyewa (CRUD)
+- Melihat data penyewaan
+- Menginput pengembalian mobil
 
 2. Penyewa
-* Mengubah profil dan password sendiri
-* Top up e-Wallet
-* Melihat data mobil
-* Menyewa mobil
-* Melihat data penyewaan sendiri
-* Membayar penyewaan
+
+- Mengubah profil dan password sendiri
+- Top up e-Wallet
+- Melihat data mobil
+- Menyewa mobil
+- Melihat data penyewaan sendiri
+- Membayar penyewaan
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,76 +1,139 @@
+/**
+ * Menangkap elemen html yang dibutuhkan.
+ */
 const imageLabel = document.querySelector('#img-label');
 const colImagePreview = document.querySelector('#col-img-preview');
 const imagePreview = document.querySelector('.img-preview');
 const image = document.querySelector('#image');
 
+// Cek apakah user(admin/penyewa) sudah meng-upload gambar.
 if (image != null) {
+    // Jika iya, beri kondisi jika gambar di-upload maka jalankan function berikut.
     image.addEventListener('change', function () {
+        // Mengambil file gambar yang diinputkan.
         const [file] = image.files;
+        // Menampilkan gambar yang diinputkan.
         imagePreview.src = URL.createObjectURL(file);
 
+        // Cek apakah ada col image preview dan iamge label.
         if (colImagePreview && imageLabel) {
+            /**
+             * Jika iya,
+             * Hapus class d-none pada col image preview.
+             * Tambahkan class d-none pada iamge label.
+             */
             colImagePreview.classList.remove('d-none');
             imageLabel.classList.add('d-none')
         }
 
+        /**
+         * Menampilkan nama file gambar pada kolom input file.
+         */
         var fileName = $(this).val().replace('C:\\fakepath\\', " ");
         $(this).next('.custom-file-label').html(fileName);
     });
 }
 
+/**
+ * Menangkap elemen html yang dibutuhkan.
+ */
 const ktpImageLabel = document.querySelector('#ktp-img-label');
 const colKtpImagePreview = document.querySelector('#col-ktp-img-preview');
 const ktpImage = document.querySelector('#ktp-image');
 const ktpImagePreview = document.querySelector('.ktp-img-preview');
 
+// Cek apakah penyewa sudah meng-upload gambar ktp.
 if (ktpImage != null) {
+    // Jika iya, beri kondisi jika gambar ktp di-upload maka jalankan function berikut.
     ktpImage.addEventListener('change', function () {
+        // Mengambil file gambar ktp yang diinputkan.
         const [file] = ktpImage.files;
+        // Menampilkan gambar ktp yang diinputkan.
         ktpImagePreview.src = URL.createObjectURL(file);
 
+        // Cek apakah ada col ktp image preview dan ktp iamge label.
         if (colKtpImagePreview && ktpImageLabel) {
+            /**
+             * Jika iya,
+             * Hapus class d-none pada col ktp image preview.
+             * Tambahkan class d-none pada ktp iamge label.
+             */
             colKtpImagePreview.classList.remove('d-none');
             ktpImageLabel.classList.add('d-none')
         }
 
+        /**
+         * Menampilkan nama file gambar ktp pada kolom input file.
+         */
         var fileName = $(this).val().replace('C:\\fakepath\\', " ");
         $(this).next('.custom-file-label').html(fileName);
     });
 }
 
+/**
+ * Menangkap elemen html yang dibutuhkan.
+ */
 const simImageLabel = document.querySelector('#sim-img-label');
 const colSimImagePreview = document.querySelector('#col-sim-img-preview');
 const simImage = document.querySelector('#sim-image');
 const simImagePreview = document.querySelector('.sim-img-preview');
 
+// Cek apakah penyewa sudah meng-upload gambar sim.
 if (simImage != null) {
+    // Jika iya, beri kondisi jika gambar sim di-upload maka jalankan function berikut.
     simImage.addEventListener('change', function () {
+        // Mengambil file gambar sim yang diinputkan.
         const [file] = simImage.files;
+        // Menampilkan gambar sim yang diinputkan.
         simImagePreview.src = URL.createObjectURL(file);
 
+        // Cek apakah ada col sim image preview dan sim iamge label.
         if (colSimImagePreview && simImageLabel) {
+            /**
+             * Jika iya,
+             * Hapus class d-none pada col sim image preview.
+             * Tambahkan class d-none pada sim iamge label.
+             */
             colSimImagePreview.classList.remove('d-none');
             simImageLabel.classList.add('d-none')
         }
 
+        /**
+         * Menampilkan nama file gambar sim pada kolom input file.
+         */
         var fileName = $(this).val().replace('C:\\fakepath\\', " ");
         $(this).next('.custom-file-label').html(fileName);
     });
 }
 
+/**
+ * Membuat function formatRupiah untuk menampilkan harga dengan format rupiah ketika user(admin/penyewa) menginput harga.
+ */
 function formatRupiah(number, prefix) {
+    // Mengganti semua inputan user(admin/penyewa) yang bukan angka dengan string kosong.
     let number_string = number.replace(/[^,\d]/g, '').toString(),
+        // Memisahkan angka diantara koma(jika ada).
         split = number_string.split(','),
+        // Menampung 3 angka terakhir sebelum koma.
         sisa = split[0].length % 3,
         rupiah = split[0].substr(0, sisa),
+        // Ketika angka ribuan, tangkap setiap 3 angka.
         ribuan = split[0].substr(sisa).match(/\d{3}/gi);
 
+    // Tambahkan titik jika yang diinput sudah ribuan
     if (ribuan) {
         separator = sisa ? '.' : '';
         rupiah += separator + ribuan.join('.');
     }
 
+    // Jika ada angka dibelakang koma.
     rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
 
+    /**
+     * Jika prefix tidak ada maka tampilkan rupiah,
+     * tapi jika prefix ada maka cek apakah ada rupiah,
+     * jika ada rupiah maka tampilkan Rp + rupiah,
+     * tapi jika tidak maka tampilkan string kosong.
+     */
     return prefix == undefined ? rupiah : (rupiah ? 'Rp' + rupiah : '');
 }
